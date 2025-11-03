cffcd67d28cbeaa45a621128486ab046ba660806
Waqar Abbas Baig <140295418+waqarbaig6133@users.noreply.github.com>
2025-11-03 10:20:23 -0500

    Refactor Honda dataset class for improved data handling

--- HondaDataset.py
+++ HondaDataset.py
@@ -1,35 +1,40 @@
 import os
 import pandas as pd
-import torch
 from torch.utils.data import Dataset
 from PIL import Image
 
-
 class Honda(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
-        self.annotations = pd.read_csv(csv_file, header=None)
+        self.data = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
 
+        # Name -> integer ID mapping
+        self.class_to_idx = {
+            'Accord': 0,
+            'City': 1,
+            'Civic': 2,
+            'Pilot': 3,
+            'Passport': 4,
+            'Nsx': 5,
+            'Odyssey': 6
+        }
+
+        # Invert dictionary for easy lookup later
+        self.idx_to_class = {v: k for k, v in self.class_to_idx.items()}
+
     def __len__(self):
-        return len(self.annotations)
+        return len(self.data)
 
-    def __getitem__(self, index):
-        # Get image path
-        img_name = self.annotations.iloc[index, 0]
+    def __getitem__(self, idx):
+        img_name = self.data.iloc[idx, 0]
+        class_name = os.path.splitext(img_name)[0].split('-')[0].strip().title()
+        label = self.class_to_idx[class_name]
+
         img_path = os.path.join(self.root_dir, img_name)
-
-        # Open image
         image = Image.open(img_path).convert("RGB")
 
-        # Handle label if present
-        if self.annotations.shape[1] > 1:
-            y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
-        else:
-            y_label = -1
-
-        # Apply transform
         if self.transform:
             image = self.transform(image)
 
-        return image, y_label
+        return image, label
